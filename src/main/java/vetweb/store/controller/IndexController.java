0bd02d40bcdfa3ba5fee419fba3075da7b91f074
Renan F Rodrigues <renanfr1047@gmail.com>
2019-03-08 10:42:57 -0300

    Setting the context path and adding view engine dependency

--- src/main/java/vetweb/store/controller/IndexController.java
+++ src/main/java/vetweb/store/controller/IndexController.java
@@ -3,16 +3,14 @@ package vetweb.store.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/home")
 public class IndexController {
 	
 	@GetMapping
-	@ResponseBody
 	public String index() {
-		return "Index page for test";
+		return "index";
 	}
 
 }
