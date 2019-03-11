e61d282dfee489d23a9912ee904625ac57442aba
Renan F Rodrigues <renanfr1047@gmail.com>
2019-03-11 17:02:52 -0300

    Test form

--- src/main/java/vetweb/store/controller/CategoryController.java
+++ src/main/java/vetweb/store/controller/CategoryController.java
@@ -1,16 +1,30 @@
 package vetweb.store.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import vetweb.store.entity.Category;
+import vetweb.store.service.CategoryService;
 
 @Controller
 @RequestMapping("/categories")
 public class CategoryController {
 	
+	@Autowired
+	private CategoryService categoryService;
+	
 	@GetMapping
-	public String save() {
+	public String saveForm() {
 		return "category/save";
+	}
+	
+	@PostMapping
+	public String save(Category category) {
+		categoryService.save(category);
+		return null;
 	}
 
 }
--- src/main/java/vetweb/store/service/CategoryService.java
+++ src/main/java/vetweb/store/service/CategoryService.java
@@ -3,10 +3,12 @@ package vetweb.store.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import vetweb.store.entity.Category;
 import vetweb.store.repository.CategoryDAO;
 
+@Service
 public class CategoryService {
 	
 	@Autowired
