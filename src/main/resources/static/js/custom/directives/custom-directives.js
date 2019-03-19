1f7f22c507e6cebd6677cd6e8966ebf7ed242cd4
Renan Rodrigues <renan.rodrigues@metasix.com.br>
2019-03-18 22:34:55 -0300

    Comment on directive definition script

--- src/main/resources/static/js/custom/directives/custom-directives.js
+++ src/main/resources/static/js/custom/directives/custom-directives.js
@@ -1,13 +1,13 @@
-angular.module('customDirectives', [])
+angular.module('customDirectives', [])//Directive module name to be included as dependency in the main module
 	.directive('categoriesTable', function() {
 
-		var ddo = {};
-		ddo.restrict = "AE";
-		ddo.transclude = true;
-		ddo.scope = {
-				h2Text: '@'
+		var ddo = {};//The directive definition object convention defines the template parameters
+		ddo.restrict = "AE";//How the elements is going to be used (As tag, tag attribute or both)
+		ddo.transclude = true;//Allows child elements to be concatenated within the directive
+		ddo.scope = {//Defines the name of directive dynamic attributes
+				h2Text: '@'//Internally using the same name as defined in the html directive invocation
         };
-		ddo.templateUrl = 'js/custom/directives/categories-table.html';
+		ddo.templateUrl = 'js/custom/directives/categories-table.html';//The html file with the tag definition
 		return ddo;
 		
 });
